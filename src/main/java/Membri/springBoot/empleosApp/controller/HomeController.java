091b78f66e71e3c00dbf92780f3a7b52b97385d8
Membri <membri_2@hotmail.com>
2020-06-15 21:06:13 +0200

    Spring MVC: agregar datos del controlador a la vista

--- src/main/java/Membri/springBoot/empleosApp/controller/HomeController.java
+++ src/main/java/Membri/springBoot/empleosApp/controller/HomeController.java
@@ -1,13 +1,20 @@
 package Membri.springBoot.empleosApp.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String mostrarHome() {
+	//Con Model podemos agregar cualquier tipo de  dato al modelo
+	public String mostrarHome(Model model) {
+		//añadimos a la variable "mensaje" la cadena "Empleos App"
+		model.addAttribute("mensaje", "Empleos App");
+		model.addAttribute("fecha", new Date());
 		return "home";
 	}
 
