a39500b849d48f1750c6b1beb9496330e8df6cfe
Membri <membri_2@hotmail.com>
2020-07-02 19:16:00 +0200

    tabla de vacantes integrada

--- src/main/java/Membri/springBoot/empleosApp/controller/HomeController.java
+++ src/main/java/Membri/springBoot/empleosApp/controller/HomeController.java
@@ -1,5 +1,7 @@
 package Membri.springBoot.empleosApp.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,10 +10,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
 import Membri.springBoot.empleos.model.Vacante;
 
 @Controller
 public class HomeController {
+	
+	@GetMapping("/tabla")
+	public String mostrarTabla(Model model) {
+		List<Vacante> lista = getVacantes();
+		model.addAttribute("vacantes", lista);
+		
+		return "tabla";
+	}
 	
 	@GetMapping("/detalles")
 	public String mostrarDetalles(Model model) {
@@ -59,6 +71,62 @@ public class HomeController {
 		model.addAttribute("vigente", vigente);
 		
 		return "home";
+	}
+	
+	/**
+	 * Metodo que devuelve una lista de objetos tipo vacante
+	 * @return
+	 * @throws java.text.ParseException 
+	 */
+	private List<Vacante> getVacantes() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		List<Vacante> lista = new LinkedList<Vacante>();
+		
+		try {
+			
+			//oferta 1
+			Vacante vacante1 = new Vacante();
+			vacante1.setId(1);
+			vacante1.setNombre("Desarrollador Web");
+			vacante1.setDescripcion("Solicitamos desarrollador web para proyecto Spring");
+			vacante1.setFecha(sdf.parse("08-02-2019"));
+			vacante1.setSalario(12.500);
+			
+			//oferta 2
+			Vacante vacante2 = new Vacante();
+			vacante2.setId(2);
+			vacante2.setNombre("Dev Ops");
+			vacante2.setDescripcion("Solicitamos un dev Ops para proyectos");
+			vacante2.setFecha(sdf.parse("08-12-2016"));
+			vacante2.setSalario(15.500);
+			
+			//oferta 3
+			Vacante vacante3 = new Vacante();
+			vacante3.setId(3);
+			vacante3.setNombre("Testing");
+			vacante3.setDescripcion("Solicitamos un QA para proyectos");
+			vacante3.setFecha(sdf.parse("08-12-2014"));
+			vacante3.setSalario(17.500);
+			
+			//oferta 4
+			Vacante vacante4 = new Vacante();
+			vacante4.setId(4);
+			vacante4.setNombre("Experto en Angular");
+			vacante4.setDescripcion("Solicitamos una persona con experiencia en Angular 9");
+			vacante4.setFecha(sdf.parse("08-12-2018"));
+			vacante4.setSalario(17.500);
+			
+			lista.add(vacante1);
+			lista.add(vacante2);
+			lista.add(vacante3);
+			lista.add(vacante4);
+			
+			
+		}catch(ParseException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		return lista;
+		
 	}
 
 }
