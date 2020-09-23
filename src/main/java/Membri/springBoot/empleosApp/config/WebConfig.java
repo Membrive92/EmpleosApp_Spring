770cfc117436def876c5365228842afbbfbba7b9
Membri <membri_2@hotmail.com>
2020-09-23 19:19:54 +0200

    ruta visible de las imagenes cambiaada con addResourceHandler (falta mejorar la proteccion con Spring Security)

---
+++ src/main/java/Membri/springBoot/empleosApp/config/WebConfig.java
@@ -0,0 +1,19 @@
+package Membri.springBoot.empleosApp.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Value("${empleosApp.ruta.imagenes}")
+	private String rutaImagenes;
+	
+@Override	
+public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	//configura la ruta visibler donde se almacenan las imagenes
+	registry.addResourceHandler("/logos/**").addResourceLocations("file"+rutaImagenes);
+}
+}
